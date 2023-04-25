ee94ad3d71c5324465b0a5a1e5cd94bac8da649b
Saigupta7887 <83281595+Saigupta7887@users.noreply.github.com>
2023-04-25 17:05:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-const http =require('http');
-const path = require('path');
-const fs = require('fs');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const cors = require("cors");
+const server = http.createServer((req, res) => {
+
+    /*
 
 
 
-  
- 
-
-const server = http.createServer((req, res)=>{
-
-    console.log(req.url);
-    // / or /api /about.html
-     
-    if( req.url ==='/'){
-     
-        fs.readFile(path.join(__dirname,'public','index.html'),
-        (err, content)=>{
-
-            if (err) throw err;
-            res.writeHead(200,{ 'Content-type': 'text/html'});
-            res.end(content);
-
-        });
+        we can Navigate to different pages via different requests.
+        if / then goto index.html
+        if /about about then goto about.html
+        if /api then laod the JSON file  /  ;) this might be something you need for your exam.
 
 
+
+    */
+
+
+    var corsOptions = {
+        origin: "*",
+    };
+    // server.use(cors());
+    if (req.url === '/') {
+        // read public.html file from public folder
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+            (err, content) => {
+
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        );
     }
-  
+
+    else if (req.url === '/about') {
+
+
+        // read the about.html file public folder
+        fs.readFile(
+            path.join(__dirname, 'public', 'about.html'),
+            (err, content) => {
+
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        );
+    }
     else if (req.url==='/api')
     {
         fs.readFile(
@@ -42,12 +64,14 @@ const server = http.createServer((req, res)=>{
     else{
         res.end("<h1> 404 nothing is here</h1>");
     }
-  
 
-   
+    /*
 
+        But what if we have  1000 pages/urls ? do we need to write 1000 if-else statements?
+
+    /*/
 });
 
-const PORT= process.env.PORT || 4552;
+const PORT= process.env.PORT || 5959;
 
 server.listen(PORT,()=> console.log(`Great our server is running on port ${PORT} `));
